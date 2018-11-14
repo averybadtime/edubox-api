42b1044461a90dcf9d00f3b7af862ac8f2adb881
Jean Carlos Ospino Gonzalez <devjc23@gmail.com>
2018-11-14 11:52:43 -0500

    Agregado e implementado CORS al proyecto

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var express = require("express")
 var app = express()
 var bodyParser = require("body-parser")
+var cors = require("cors")
+
+var usersRoutes = require("./routes/users")
 
 var PORT = 3000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+
+app.use("/api", usersRoutes)
 
 app.listen(PORT, function () {
 	console.log("Server started successfully!")
